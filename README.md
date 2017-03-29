3b1846b2d74b8abe57052aa3ad1a61c61ee791dc
GuillaumeUnice <guillaume.unice@gmail.com>
2017-03-29 14:07:23 +0200

    Add Worker dependency + implement updateVote

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 docker run -d -p 6379:6379 redis:alpine
 
-docker run --name some-postgres -e POSTGRES PASSWORD=mysecretpassword -d postgres
-
+docker run --name postgres --net host -e POSTGRES_USER=postgres -d postgres
 
--- worker/src/main/java/fr/treeptik/vertx/worker/WorkerVerticle.java
+++ worker/src/main/java/fr/treeptik/vertx/worker/WorkerVerticle.java
@@ -3,55 +3,131 @@ package fr.treeptik.vertx.worker;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.PostgreSQLClient;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.sql.UpdateResult;
 import io.vertx.redis.RedisClient;
 import io.vertx.redis.RedisOptions;
 
-
 public class WorkerVerticle extends AbstractVerticle {
+    private SQLConnection connection;
+    private  RedisClient redis;
 
     @Override
     public void start(Future<Void> future) {
+        redis = RedisClient.create(vertx, new RedisOptions().setHost("127.0.0.1"));
 
-        vertx.setPeriodic(3000, id -> {
-            // This handler will get called every second
-            RedisClient redis = RedisClient.create(vertx, new RedisOptions().setHost("127.0.0.1"));
+        JsonObject postgreSQLClientConfig = new JsonObject().put("host", "localhost");
+        postgreSQLClientConfig.put("database", "postgres")
+                .put("username", "postgres");
 
-            redis.blpop("vote", 2, res -> {
+        AsyncSQLClient postgreSQLClient = PostgreSQLClient.createShared(vertx, postgreSQLClientConfig);
 
-                if (res.succeeded()) {
-                    if (res.result()!=null){
-                        //vertx.eventBus().send("data",res.result());
-                        System.out.println(res.result());
+
+        postgreSQLClient.getConnection(conn -> {
+            if (conn.succeeded()) {
+                this.connection = conn.result();
+                System.out.println("Connection POSTGRESQL OK");
+                String sql = "CREATE TABLE IF NOT EXISTS votes (id SERIAL PRIMARY KEY," +
+                        "vote_id varchar(255), vote varchar(255));";
+
+                this.connection.execute(sql, execute -> {
+                    if(execute.succeeded()) {
+                        System.out.println("Table votes created !");
+                        this.getVotes();
                     } else {
-                        System.out.println("RESULT null");
+                        System.out.println("Creation table Failed" + execute.cause());
                     }
-                } else {
-                    System.out.println("Connection or Operation Failed " + res.cause());
-                }
-            });
+                });
+
+            } else {
+                System.out.println("Connection or Operation Failed " + conn.cause());
+            }
         });
-        // If a config file is set, read the host and port.
-//        String host = Vertx.currentContext().config().getString("host");
-//        if (host == null) {
-//            host = "127.0.0.1";
-//        }
+//        final JDBCClient jdbc = JDBCClient.createNonShared(vertx, new JsonObject()
+//                .put("url", "jdbc:postgresql://127.0.0.1:5432/postgres")
+//                .put("user", "postgres")
+////                .put("password", "abc2015")
+//        );
+//        System.out.println("Connection or Operation Failed ");
+//        jdbc.getConnection(res -> {
 //
-//        // Create the redis client
-//        final RedisClient client = RedisClient.create(vertx,
-//                new RedisOptions().setHost(host));
+//            if (res.succeeded()) {
+//                SQLConnection connection = res.result();
 //
-//        client.set("key", "value", r -> {
-//            if (r.succeeded()) {
-//                System.out.println("key stored");
-//                client.get("key", s -> {
-//                    System.out.println("Retrieved value: " + s.result());
-//                });
+//                System.out.println("RESULT null");
 //            } else {
-//                System.out.println("Connection or Operation Failed " + r.cause());
+//                System.out.println("Connection or Operation Failed " + res.cause());
 //            }
 //        });
     }
 
+    public void updateVote(String voteId, String vote) {
 
+//        JsonObject postgreSQLClientConfig = new JsonObject().put("host", "127.0.0.1");
+//        AsyncSQLClient postgreSQLClient = PostgreSQLClient.createShared(vertx, postgreSQLClientConfig);
+//
+//        postgreSQLClient.getConnection(res -> {
+//            if (res.succeeded()) {
+//                SQLConnection connection = res.result();
+//
+//                System.out.println("RESULT null");
+//            } else {
+//                System.out.println("Connection or Operation Failed " + res.cause());
+//            }
+//        });
+
+        String deletePreviousVote = "DELETE FROM votes WHERE vote_id=?";
+        JsonArray deleteParams = new JsonArray().add(voteId);
+        this.connection.updateWithParams(deletePreviousVote, deleteParams, res -> {
+           if(res.succeeded()) {
+               UpdateResult updateResult = res.result();
+               System.out.println("Delete previous vote: " + updateResult.getUpdated());
+
+               String insert = "INSERT INTO votes(vote_id, vote) VALUES(?, ?)";
+               JsonArray insertParams = new JsonArray()
+                       .add(voteId)
+                       .add(vote);
+               this.connection.updateWithParams(insert, insertParams, insertRes -> {
+                  if(insertRes.succeeded()) {
+                      UpdateResult insertResult = insertRes.result();
+                      System.out.println("Insert vote: " + insertResult.getUpdated());
+                  } else {
+                      System.out.println("Error insert vote " + insertRes.cause());
+                  }
+               });
+           } else {
+               System.out.println("Error delete previous vote " + res.cause());
+           }
+        });
+
+    }
+
+    public void getVotes() {
+        vertx.setPeriodic(1000, id -> {
+            // This handler will get called every second
+            redis.blpop("vote", 2, res -> {
+                if (res.succeeded()) {
+                    if (res.result()!=null){
+                        System.out.println(res.result().getString(1));
+                        JsonObject voteData = new JsonObject(res.result().getString(1));
+                        String voteId = voteData.getString("vote_id");
+                        String vote = voteData.getString("vote");
+                        this.updateVote(voteId, vote);
+                    }
+//                    else {
+//                        System.out.println("RESULT null");
+//                    }
+                } else {
+                    System.out.println("Connection or Operation Failed " + res.cause());
+                }
+            });
+
+        });
+    }
 
 }
