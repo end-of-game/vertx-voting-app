6ab9ff8047d235a7b5742a013b3f25a008b43cf5
GuillaumeUnice <guillaume.unice@gmail.com>
2017-04-10 11:29:02 +0200

    enable prod mode

--- result/frontend/src/main.ts
+++ result/frontend/src/main.ts
@@ -3,7 +3,7 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-if (environment.production) {
+if (environment.production || !/localhost/.test(document.location.host)) {
   enableProdMode();
 }
 
