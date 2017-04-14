ad0a8469f675754558ac7b82093e1f2b32d4a36b
GuillaumeUnice <guillaume.unice@gmail.com>
2017-04-14 16:13:02 +0200

    Add JWT inside cookie

--- vote/src/main/java/fr/treeptik/vertx/vote/ServerVerticle.java
+++ vote/src/main/java/fr/treeptik/vertx/vote/ServerVerticle.java
@@ -1,11 +1,17 @@
 package fr.treeptik.vertx.vote;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTOptions;
 import io.vertx.ext.web.Cookie;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -28,6 +34,12 @@ public class ServerVerticle extends AbstractVerticle {
     private static final String VOTE = "vote";
     private static final String HOSTNAME = "hostname";
 
+    JsonObject config = new JsonObject().put("keyStore", new JsonObject()
+            .put("path", "keystore.jceks")
+            .put("type", "jceks")
+            .put("password", "secret"));
+
+    JWTAuth provider = null;
     @Override
     public void start() throws Exception {
         final Router router = Router.router(vertx);
@@ -53,6 +65,7 @@ public class ServerVerticle extends AbstractVerticle {
         StaticHandler staticHandler = StaticHandler.create();
         staticHandler.setCachingEnabled(false);
         router.route("/*").handler(StaticHandler.create());
+        this.provider = JWTAuth.create(vertx, this.config);
 
         vertx.createHttpServer()
                 .requestHandler(router::accept)
@@ -60,49 +73,61 @@ public class ServerVerticle extends AbstractVerticle {
     }
 
     private void info (RoutingContext context) {
-        checkID(context);
-
-        JsonObject response = new JsonObject();
-        response.put(HOSTNAME, "zepouetstation");
-        JsonArray choice = new JsonArray();
-        choice.add("Cats");
-        choice.add("Dogs");
-        response.put("choice", choice);
-
-        context.response()
-                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                .end(response.toString());
+        checkID(context).setHandler(result -> {
+            JsonObject response = new JsonObject();
+            response.put(HOSTNAME, "zepouetstation");
+            JsonArray choice = new JsonArray();
+            choice.add("Cats");
+            choice.add("Dogs");
+            response.put("choice", choice);
+            context.response()
+                    .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .end(response.toString());
+        });
     }
 
     private void vote (RoutingContext context) {
-
         JsonObject requestBody = context.getBodyAsJson();
         final String vote = requestBody.getString(VOTE);
-
         RedisClient redis = RedisClient.create(vertx, new RedisOptions().setHost("vds-redis"));
         JsonObject response = new JsonObject();
-        response.put(VOTE_ID, checkID(context));
-        response.put(VOTE, vote);
-        System.out.println(response.toString());
-        redis.rpush("vote", response.toString(), r -> {
-            if (r.succeeded()) {
-                System.out.println("key stored");
-            } else {
-                System.out.println("Connection or Operation Failed " + r.cause());
+        checkID(context).setHandler(result -> {
+            if(result.failed()) {
+                result.cause();
             }
+            response.put(VOTE_ID, result.result().toString());
+            response.put(VOTE, vote);
+            redis.rpush("vote", response.toString(), r -> {
+                if (r.succeeded()) {
+                    System.out.println("key stored");
+                } else {
+                    System.out.println("Connection or Operation Failed " + r.cause());
+                }
+            });
+            context.response()
+                    .putHeader(HttpHeaders.CONTENT_TYPE, "application/json").end(response.toString());
         });
-        context.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json").end(response.toString());
     }
 
-    private String checkID(RoutingContext context) {
+    private Future<String> checkID(RoutingContext context) {
+        Future<String> future = Future.future();
         Cookie cookie = context.getCookie(VOTE);
+
         if (cookie != null) {
-            return cookie.getValue();
+            JsonObject authInfo = new JsonObject().put("jwt", cookie.getValue());
+            this.provider.authenticate(authInfo, res -> {
+                if (res.failed()) {
+                    future.fail(res.cause());
+                }
+                future.complete((String) res.result().principal().getValue("sub"));
+            });
         } else {
             SecureRandom random = new SecureRandom();
             String voteId = new BigInteger(130, random).toString(16).substring(0, 16);
-            context.addCookie(Cookie.cookie(VOTE, voteId));
-            return voteId;
+            String token = this.provider.generateToken(new JsonObject().put("sub", voteId), new JWTOptions().setAlgorithm("RS256").setExpiresInMinutes(60L));
+            context.addCookie(Cookie.cookie(VOTE, token));
+            future.complete(voteId);
         }
+        return future;
     }
 }
